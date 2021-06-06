8f49e27a3a4e086743b27fb8c75fa6c4111285c7
Folajomi Shotunde <40181350+jormee@users.noreply.github.com>
2021-06-06 20:36:30 +0100

    FEAT: Add table of Content to blog posts pages

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -3,6 +3,7 @@ import { graphql } from 'gatsby'
 import { GatsbyImage, getImage} from 'gatsby-plugin-image'
 
 import Layout from '../components/layout.js'
+
 import '../styles/config.scss'
 import '../styles/blogPost.scss'
 
@@ -10,6 +11,7 @@ export const query = graphql`
   query postQuery($slug: String){
     contentfulBlogPost(slug: { eq: $slug}) {
       title
+      published(formatString: "Do MMM, YYYY")
       coverImg {
         description
         gatsbyImageData(
@@ -20,6 +22,8 @@ export const query = graphql`
       childContentfulBlogPostPostBodyTextNode {
         childMarkdownRemark {
           html
+          timeToRead
+          tableOfContents
         }
       }
     }
@@ -27,19 +31,30 @@ export const query = graphql`
 `
 
 const BlogPost = ({ data }) => {
-  const { title, childContentfulBlogPostPostBodyTextNode, coverImg } = data.contentfulBlogPost
+  const { title, childContentfulBlogPostPostBodyTextNode, coverImg, published } = data.contentfulBlogPost
  
-  const { html } = childContentfulBlogPostPostBodyTextNode.childMarkdownRemark
+  const { html, timeToRead, tableOfContents } = childContentfulBlogPostPostBodyTextNode.childMarkdownRemark
   const cover = getImage(coverImg)
   return(
     <Layout>
       <article className="post">
         <h1 className="post-title">{title}</h1>
+        <p className="post-details">
+          {
+            `${published} • ${timeToRead}mins read`
+          }
+        </p>
         <GatsbyImage image={cover} alt={coverImg.description} />
-        <div 
-          className="post-body"
-          dangerouslySetInnerHTML={{__html: html}}
-        >          
+        <div className="post-content">
+          <div className="ToC">
+            <h3>Table of Contents</h3>
+            <div dangerouslySetInnerHTML={{__html: tableOfContents}}></div>
+          </div>
+          <div 
+            className="post-body"
+            dangerouslySetInnerHTML={{__html: html}}
+          >          
+          </div>
         </div>
       </article>
     </Layout>
